9a3158daa46ef7530ece2dd0c0a3b15077f278d4
NathanCarr10 <nathancarr18@gmail.com>
2024-10-06 20:20:41 +0100

    added Layout function to render NavBar, Footer and Header components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,49 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Content from './components/Content';
 
-function App() {
+/**
+ * Layout component is responsible for rendering the NavigationBar,
+ * Footer and Header based on the current URL,
+ * and the routes that define different pages of the app.
+ */
+function Layout() {
+  // useLocation hook is used to access the current URL path
+  const location = useLocation();
+
   return (
-    <Router>
+    <>
+      {/* The NavigationBar is rendered at the top for all routes */}
       <NavigationBar />
+
+      {/* Render the Footer component only when the path is '/read' */}
+      {location.pathname === '/read' && <Footer />}
+
+      {/* Render the Header component only when the path is '/create' */}
+      {location.pathname === '/create' && <Header />}
+
+      {/* Define routes for the application */}
       <Routes>
         <Route path="/home" element={<Content />} />
+        
         <Route path="/read" element={<h1>Read Component</h1>} />
+        
         <Route path="/create" element={<h1>Create Component</h1>} />
       </Routes>
-      <Footer />
+    </>
+  );
+}
+
+/**
+ * App component is the main component of the application.
+ */
+function App() {
+  return (
+    // Router provides routing functionality to the entire application.
+    <Router>
+      <Layout />
     </Router>
   );
 }
