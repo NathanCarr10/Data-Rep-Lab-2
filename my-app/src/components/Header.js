025a94244818fd78f2250dc29f1e4bb5ea5c4626
NathanCarr10 <nathancarr18@gmail.com>
2024-10-06 20:24:25 +0100

    added comments to Header.js

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,4 +1,10 @@
+/**
+ * Header component is a simple functional component
+ * that displays a header message.
+ */
+
 const Header = () => {
+  // Renders a heading (h1) containing the header text
     return <h1>My Header in another component</h1>;
   };
   
