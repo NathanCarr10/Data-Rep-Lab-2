7b9525d4f6f07f31e522c664c01f0be273a79662
NathanCarr10 <nathancarr18@gmail.com>
2024-10-06 20:23:26 +0100

    added comments to footer.js

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,4 +1,10 @@
+/**
+ * Footer component is a simple functional component
+ * that displays a footer message.
+ */
+
 const Footer = () => {
+  // Renders a heading (h3) containing the footer text
     return <h3>My Footer in another component</h3>;
   };
   
