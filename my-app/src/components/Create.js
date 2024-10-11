8cd49441cd0ddf2a8a6b74c4dc6fc896cd4c0a3c
NathanCarr10 <[nathancarr18@gmail.com]>
2024-10-11 19:04:25 +0000

    added comments to Create.js file

--- my-app/src/components/Create.js
+++ my-app/src/components/Create.js
@@ -1,8 +1,11 @@
+// Create component definition
 const Create = ()=>{
     return (
         <div>
+            {/* Displaying a heading */}
            <h3> Hello from Create component </h3> 
         </div>
     )
 }
+// Exporting the Create component to be used in other parts of the application
 export default Create;
