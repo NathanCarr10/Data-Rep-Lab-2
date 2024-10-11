beff450c0915cce932f7abfda662d3748c5912fa
NathanCarr10 <[nathancarr18@gmail.com]>
2024-10-11 19:15:41 +0000

    added comments to movies,js file

--- my-app/src/components/Movies.js
+++ my-app/src/components/Movies.js
@@ -1,12 +1,16 @@
+//Importing the MovieItem component
 import MovieItem from "./movieitem";
 
+//Movies component that takes props as an argument
 const Movies = (props )=>{
-
+    //Mapping through the myMovies prop, which is an array of movie objects
     return props.myMovies.map(
         (movie)=>{
+            //Return a MovieItem component for each movie in array
             return <MovieItem mymovie={movie} key={movie.imdbID}/>
         }
 
     );
 }
+//Exporting the Movies component so it can be used in other parts of the app
 export default Movies;
