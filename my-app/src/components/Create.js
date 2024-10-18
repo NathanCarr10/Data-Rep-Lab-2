d09ae96210b107fb4775684254e74f935720635e
NathanCarr10 <[nathancarr18@gmail.com]>
2024-10-18 14:32:34 +0000

    added comments to read.js

--- my-app/src/components/Create.js
+++ my-app/src/components/Create.js
@@ -1,47 +1,51 @@
-import {useState} from "react";
+import { useState } from "react";
 
 // Create component definition
-const Create = ()=>{
-    
-    const [title, setTitle] = useState('');
-    const [movieYear, setmovieYear] = useState ('');
+const Create = () => {
 
-    const handleSubmit =(e)=>{
+
+    const [title, setTitle] = useState('');
+    const [movieYear, setmovieYear] = useState('');
+
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log(title);
     }
 
     return (
         <div>
-          <h2>This is my Create Component.</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label>Add Movie Title: </label>
-              <input type="text"
-                className="form-control"
-                value={title}
-                onChange={(e) => { setTitle(e.target.value) }}
-              />
-              <label>Add Movie Year: </label>
-              <input type="number"
-                className="form-control"
-                value={title}
-                onChange={(e) => { setTitle(e.target.value) }}
-              />
+            <h2>This is my Create Component.</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Movie Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                    <div className="form-group">
+                        <label>Add Movie Year: </label>
+                        <input type="number"
+                            className="form-control"
+                            value={title}
+                            onChange={(e) => { setTitle(e.target.value) }}
+                        />
+                    </div>
 
-              <label>Add Movie Poster: </label>
-              <input type="text"
-                className="form-control"
-                value={title}
-                onChange={(e) => { setTitle(e.target.value) }}
-              />
+                    <div className="form-group">
+                        <label>Add Movie Poster: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={title}
+                            onChange={(e) => { setTitle(e.target.value) }}
+                        />
+                    </div>
 
-
-            </div>
-            <input type="submit" value="Add Movie" />
-          </form>
+                </div>
+                <input type="submit" value="Add Movie" />
+            </form>
         </div>
-      );
-    }
+    );
+}
 // Exporting the Create component to be used in other parts of the application
 export default Create;
