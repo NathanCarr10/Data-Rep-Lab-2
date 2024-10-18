054249752441e133c797367e9074fd1ef67a0eb3
NathanCarr10 <[nathancarr18@gmail.com]>
2024-10-18 14:38:04 +0000

    added comments to create.js

--- my-app/src/components/Create.js
+++ my-app/src/components/Create.js
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import { useState } from "react"; //useState is React Hook that allows you to add state to a functional component
 
 // Create component definition
 const Create = () => {
 
-
+    //useState is React Hook that allows you to add state to a functional component
     const [title, setTitle] = useState('');
     const [movieYear, setmovieYear] = useState('');
+    const [moviePoster, setmoviePoster] = useState('');
+    
 
+    //this allows you to handle the submissions
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(title);
+        console.log(movieYear);
+        console.log(moviePoster);
     }
-
+    //user enters values for movie title, movie year and movie poster url
     return (
         <div>
             <h2>This is my Create Component.</h2>
@@ -27,8 +32,8 @@ const Create = () => {
                         <label>Add Movie Year: </label>
                         <input type="number"
                             className="form-control"
-                            value={title}
-                            onChange={(e) => { setTitle(e.target.value) }}
+                            value={movieYear}
+                            onChange={(e) => { setmovieYear(e.target.value) }}
                         />
                     </div>
 
@@ -36,8 +41,8 @@ const Create = () => {
                         <label>Add Movie Poster: </label>
                         <input type="text"
                             className="form-control"
-                            value={title}
-                            onChange={(e) => { setTitle(e.target.value) }}
+                            value={moviePoster}
+                            onChange={(e) => { setmoviePoster(e.target.value) }}
                         />
                     </div>
 
--- my-app/src/components/Read.js
+++ my-app/src/components/Read.js
@@ -3,7 +3,7 @@ import Movies from "./Movies"
 import { useEffect, useState } from "react"; //By using this Hook, you tell React that your component needs to do something after render
 import axios from "axios"; //Axios helps developers make HTTP requests from NodeJS or XMLHttpRequests from a browser.
 const Read = () => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([])// useState is React Hook that allows you to add state to a functional component
   useEffect(() => {
     axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
       .then((response) => { // Axios request objects have a `then()` function, which means you can use them with promise chains and async/await.
